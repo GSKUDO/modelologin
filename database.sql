1069546c2a3449bdcc5e16f9e393bdd8eb9c1170
Gislene Sayuri <gskudo@yahoo.com.br>
2022-04-19 20:42:37 -0300

    Add files via upload

--- database.sql
+++ database.sql
@@ -1,20 +1,19 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE DATABASE jwtusers;
+CREATE DATABASE jwt;
 
 CREATE TABLE users(
-  user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-  user_name TEXT NOT NULL,
-  user_email TEXT NOT NULL UNIQUE,
-  user_password TEXT NOT NULL
+    user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_name TEXT NOT NULL,
+    user_email TEXT NOT NULL,
+    user_password TEXT NOT NULL
 );
 
 SELECT * FROM users;
 
-INSERT INTO users (user_name,user_email,user_password) VALUES ('Bob','bob@email.com','bob');
+INSERT INTO users (user_name, user_email, user_password) VALUES ('Bob', 'bob@email.com', 'bob');
 
-
---psql -U postgres
---\c jwtusers
---\dt
---heroku pg:psql
+--psql -U postgres 
+-- \c jwt
+-- \dt 
+-- heroku pg:psql
--- db.js
+++ db.js
@@ -2,19 +2,14 @@ import pg from 'pg';
 const { Pool } = pg;
 
 let localPoolConfig = {
-  user: 'postgres',
-  password: 'gislaine1',
-  host: 'localhost',
-  port: '5432',
-  database: 'jwtusers'
+    user: 'postgres',
+    password: 'gislaine1',
+    host: 'localhost',
+    port: '5432',
+    database: 'jwt'
 };
 
-const poolConfig = process.env.DATABASE_URL ? {
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-} : localPoolConfig;
+const poolConfig = process.env.DATABASE_URL ? {connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false}} : localPoolConfig;
 
 const pool = new Pool(poolConfig);
 export default pool;
